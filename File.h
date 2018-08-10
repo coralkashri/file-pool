6e5803dc3b699bdd1fee60b55e2b0f6ffcf2aea3
Sherlock <korel@korel>
2018-08-11 02:33:55 +0300

    Added enums to File.

--- File.h
+++ File.h
@@ -6,11 +6,37 @@
 #define FILESAPI_FILE_H
 
 #include <iostream>
+#include <mutex>
+
+/**
+ * >> if file_mode is OPEN_IN_ACTION:
+ * SINGLE_AND_DONE - read/write single time, and then close the file.
+ * SINGLE_AND_MORE - read/write single time, but don't close the file yet. wait for SINGLE_AND_DONE mode.
+ * MULTIPLE - close the file only in programmer order, until then the file will be remain open.
+ */
+enum class ReadWriteMode {
+    SINGLE_AND_DONE,
+    SINGLE_AND_MORE,
+    MULTIPLE,
+    DONE
+};
+
+enum class FileMode {
+    ALWAYS_OPEN,
+    OPEN_IN_ACTION
+};
 
 class File {
 private:
     std::string         name;
+    FileMode            file_mode;
+    ReadWriteMode       read_write_mode;
+    std::mutex          read_write_mutex;
+
+    void change_rwm(ReadWriteMode new_rwm); // Change read/write mode
+
 public:
+    File(const std::string &file_path);
 };
 
 
--- FilesManager.h
+++ FilesManager.h
@@ -11,11 +11,12 @@
 
 class FilesManager {
 private:
-    std::vector<File>         files;
-
+    std::vector<File>           files;
+    size_t                      max_files; // zero for unlimited
+    std::string                 files_path; // Leave "" if there is no single path for all of the files
 
 public:
-
+    FilesManager(size_t max_files, const std::string &files_path);
     File& get(size_t number);
 };
 
